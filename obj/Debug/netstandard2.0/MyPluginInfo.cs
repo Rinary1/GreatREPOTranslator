a01f06105a23471801af8132eced9921b6b8895f
Rinary <rinary.super@gmail.com>
2025-06-29 20:13:12 +0300

    Delete obj directory

--- obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace REPO_Translator
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "REPO_Translator";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- obj/Release/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace REPO_Translator
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "REPO_Translator";
-        public const string PLUGIN_NAME = "REPO_Translator";
-        public const string PLUGIN_VERSION = "1.2";
-    }
-}
